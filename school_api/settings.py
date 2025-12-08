08f83f781ce455660deaa832f138fadb6c8c362f
Ashit Rai <ashit.rai@ab-inbev.com>
2025-12-09 00:49:27 +0530

    revert: change STATIC_URL and MEDIA_URL back to root paths
    - Revert STATIC_URL from /api/static/ to /static/
    - Revert MEDIA_URL from /api/media/ to /media/
    - cPanel Python App handles /api/ prefix separately
    - Static files should be at root level for cPanel routing

--- school_api/settings.py
+++ school_api/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/api/static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Additional locations of static files
@@ -157,7 +157,7 @@ STATICFILES_FINDERS = [
 ]
 
 # Media files
-MEDIA_URL = "/api/media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
